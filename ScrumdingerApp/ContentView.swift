9e9ead338e671cc7c121cc5271fed852aa22b6a8
Yuriy Taranov <ig.seldom@gmail.com>
2021-12-06 04:12:29 +0300

    Initial Commit

---
+++ ScrumdingerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrumdingerApp
+//
+//  Created by Yuiry Taranov on 06.12.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrumdingerApp/ScrumdingerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerAppApp.swift
+//  ScrumdingerApp
+//
+//  Created by Yuiry Taranov on 06.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
